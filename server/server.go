0b58fb5faf001a4ebf805dc352c7dc56725a5a14
Avi-0009 <prakharsahu1229@gmail.com>
2026-05-15 14:00:01 +0530

    Cors issue resolved now working for other devices, same network

--- server/server.go
+++ server/server.go
@@ -12,7 +12,10 @@ func NewServer() *gin.Engine {
 
 	// 1. Add CORS Middleware right here
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, // Your Vite frontend URL
+		AllowOrigins: []string{
+			"http://localhost:5173",
+			"http://192.168.0.236:5173", // Add your phone's frontend origin here
+		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
