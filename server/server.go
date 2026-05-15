b32a028e367a58ad6ba37ed5ca82f0692e18af27
Avi-0009 <prakharsahu1229@gmail.com>
2026-05-16 02:11:07 +0530

    testing api calls

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"time"
 
+	"github.com/Avi-0009/InstantWicket-backend/config"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -13,15 +14,40 @@ func NewServer() *gin.Engine {
 	// 1. Add CORS Middleware right here
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
-			"http://localhost:5173",
-			//"http://192.168.0.236:5173", // Add your phone's frontend origin here
-			"http://192.168.29.139:5173",
+			config.GetEnv(
+				"LOCAL_URL",
+				"http://localhost:5173",
+			),
+
+			config.GetEnv(
+				"FRONTEND_URL",
+				"http://192.168.29.139:5173",
+			),
 		},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
+
+		AllowMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"PATCH",
+			"DELETE",
+			"OPTIONS",
+		},
+
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Type",
+			"Accept",
+			"Authorization",
+		},
+
+		ExposeHeaders: []string{
+			"Content-Length",
+		},
+
 		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+
+		MaxAge: 12 * time.Hour,
 	}))
 
 	r.Use(gin.Recovery())
