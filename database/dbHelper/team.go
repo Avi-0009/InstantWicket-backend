7dbac7e6cc67ba6d569f6d54bcb6b6fa1f81dcf4
Avi-0009 <prakharsahu1229@gmail.com>
2026-05-21 00:00:02 +0530

    minor api bug in team creation fixed

--- database/dbHelper/team.go
+++ database/dbHelper/team.go
@@ -7,7 +7,7 @@ import (
 
 func CreateTeam(name, createdBy string) (string, error) {
 	var teamID string
-	query := `INSERT INTO matches (name, created_by) VALUES ($1, $2) RETURNING id`
+	query := `INSERT INTO teams (name, created_by) VALUES ($1, $2) RETURNING id`
 	err := database.DB.Get(&teamID, query, name, createdBy)
 	if err != nil {
 		return "", err
